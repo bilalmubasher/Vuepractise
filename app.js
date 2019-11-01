89a2c656c03fc1b7bd9bfe774ea94b2a6848cb82
bilal <bilalmubasher514@gmail.com>
2019-11-01 04:34:56 -0700

    Two way data binding part-8

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 new Vue({
     el:'#vue-app',
     data:{
-        age:'25',
-        x:0,
-        y:0
+        name:'',
+        age:''
     },
     methods: {
-        logName:function()
-        {
-            console.log('enter your name');
-        }
-
+        
     },
 });
